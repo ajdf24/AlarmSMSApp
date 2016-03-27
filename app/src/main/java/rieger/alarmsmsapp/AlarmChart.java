b7827ed32bc5d7d46668e37f82da34c1329462d6
Sebastian <sebastian2406@t-online.de>
2016-03-27 16:25:30 +0200

    Speichern des Charts eingefügt!
    Liste mit erhaltenen Nachrichten begonnen.

--- app/src/main/java/rieger/alarmsmsapp/AlarmChart.java
+++ app/src/main/java/rieger/alarmsmsapp/AlarmChart.java
@@ -1,13 +1,21 @@
 package rieger.alarmsmsapp;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.os.AsyncTask;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.data.BarData;
@@ -20,25 +28,47 @@ import com.github.mikephil.charting.utils.ColorTemplate;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 import rieger.alarmsmsapp.control.observer.MessageObserver;
 import rieger.alarmsmsapp.model.Message;
+import rieger.alarmsmsapp.util.AppConstants;
 
 public class AlarmChart extends AppCompatActivity {
 
     private AdView adView;
+
+    private Button saveChart;
+
+    private BarChart chart;
+
+    private View layoutView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarm_chart);
 
+        initializeGUI();
+
+        initializeActiveElements();
+
+    }
+
+    private void initializeGUI() {
+        layoutView = findViewById(R.id.activity_alarm_chart);
+
         adView = (AdView) findViewById(R.id.adView);
+        chart = (BarChart)findViewById(R.id.chart);
+        saveChart = (Button) findViewById(R.id.activity_alarm_chart_save_chart);
+    }
+
+    private void initializeActiveElements() {
         AdRequest adRequest = new AdRequest.Builder().build();
         if(adView != null){
-
             adView.loadAd(adRequest);
         }
 
@@ -47,6 +77,34 @@ public class AlarmChart extends AppCompatActivity {
         LoadStatisticTask task = new LoadStatisticTask();
         task.execute(messageList);
 
+        saveChart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ActivityCompat.checkSelfPermission(AlarmChart.this, Manifest.permission.READ_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(AlarmChart.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                    AppConstants.PermissionsIDs.PERMISSION_ID_FOR_STORAGE);
+                }
+                chart.saveToGallery("Alarm_Chart_" + new SimpleDateFormat("yyyyMMddHHmmss").format(Calendar.getInstance().getTime()) + ".jpg", 100);
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[], @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case AppConstants.PermissionsIDs.PERMISSION_ID_FOR_STORAGE:
+                if (grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+
+                    Snackbar snackbar = Snackbar
+                            .make(layoutView, R.string.toast_permission_storage_denied, Snackbar.LENGTH_LONG);
+
+                    View snackbarView = snackbar.getView();
+                    TextView textView = (TextView) snackbarView.findViewById(android.support.design.R.id.snackbar_text);
+                    textView.setTextColor(Color.WHITE);
+                    snackbar.show();
+                }
+                break;
+        }
     }
 
     @Override
@@ -124,6 +182,7 @@ public class AlarmChart extends AppCompatActivity {
             ProgressBar progressBar = (ProgressBar) findViewById(R.id.activity_alarm_chart_progressBar);
 
             progressBar.setVisibility(View.INVISIBLE);
+            saveChart.setEnabled(true);
 
             BarDataSet dataset = new BarDataSet(result, "");
 
@@ -141,7 +200,6 @@ public class AlarmChart extends AppCompatActivity {
             labels.add("November");
             labels.add("December");
 
-            BarChart chart = (BarChart)findViewById(R.id.chart);
             chart.setVisibility(View.VISIBLE);
 
             BarData data = new BarData(labels, dataset);
@@ -158,6 +216,7 @@ public class AlarmChart extends AppCompatActivity {
                 @Override
                 public void onValueSelected(Entry e, int dataSetIndex, Highlight h) {
                     Log.d("Chart", "Entry: " + e.getXIndex());
+
                 }
 
                 @Override
